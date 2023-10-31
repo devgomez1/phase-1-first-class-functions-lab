dc36e2fb964249a3d94410bb29ee0c791f041e16
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-10-31 14:49:28 -0500

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2)
+};
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(-2)
+};
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+];
+
+function createFareMultiplier(integer) {
+return function(fare) {
+    return fare * integer;
+  }
+};
+
+const fareDoubleDouble = createFareMultiplier(2);
+const fareDoubler = fareDoubleDouble;
+const fareTripleTriple = createFareMultiplier(3);
+const fareTripler = fareTripleTriple; 
+
+function selectDifferentDrivers(drivers, fn) {
+     const selectedDrivers = fn(drivers);
+     return selectedDrivers;
+      }
